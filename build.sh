fc52f3caf344a34728d9804ec23b12c23be76e10
Karl Hegbloom <karl.hegbloom@gmail.com>
2017-11-10 15:53:33 -0700

    Remove trailing - from SIGNED_STUB to improve jm-sh/releases.sh

--- build.sh
+++ build.sh
@@ -15,8 +15,7 @@ BRANCH="propachi-texmacs-master"
 CLIENT="propachi-texmacs"
 VERSION_ROOT="1.1."
 
-# propachi_monkey_patch_for_zotero_csl_processor_and_editor_integration_with_texmacs_support-1.1.113beta8-fx.xpi
-SIGNED_STUB="propachi_monkey_patch_for_zotero_csl_processor_and_editor_integration_with_texmacs_support-"
+SIGNED_STUB="propachi_monkey_patch_for_zotero_csl_processor_and_editor_integration_with_texmacs_support"
 
 # citeproc-js is a symlink to a git checkout containing my
 # fork of the citeproc-js with appropriate patches applied.
@@ -26,22 +25,20 @@ function xx-fetch-latest-processor () {
     cd ../citeproc-js
     ./test.py -B
     cp citeproc.js "${SCRIPT_DIR}/chrome/content/citeproc.js"
-    # sed -si 's/this\.development_extensions\.main_title_from_short_title = false/this\.development_extensions\.main_title_from_short_title = true/' "${SCRIPT_DIR}/chrome/content/citeproc.js"
-    # sed -si 's/this\.development_extensions\.uppercase_subtitles = false/this\.development_extensions\.uppercase_subtitles = true/' "${SCRIPT_DIR}/chrome/content/citeproc.js"
     cd "${SCRIPT_DIR}"
 }
 
-# The integration.js is also in a git checkout containing my fork and
-# branch of the Juris-M zotero code with my patches applied. The goal
-# will be to have the changes become part of the upstream program, but
-# for now, in order to avoid having people need to run from a git
-# clone, I'll pull it here and monkey-patch it for them on top of the
-# released versin of Juris-M. That way it's just a matter of
-# installing a Firefox xpi package and they'll be able to start using
-# the TeXmacs / Juris-M / Zotero integration.
+# The integration.js is also in a git checkout containing my fork and branch of
+# the Juris-M zotero code with my patches applied. The goal will be to have the
+# changes become part of the upstream program, but for now, in order to avoid
+# having people need to run from a git clone, I'll pull it here and
+# monkey-patch it for them on top of the released version of Juris-M. That way,
+# it's just a matter of installing a Firefox xpi package and they'll be able to
+# start using the TeXmacs / Juris-M / Zotero integration.
 #
 # This must be maintained separately to avoid attempting to redefine
 # constants.
+#
 #function xx-fetch-latest-integration-js () {
 #    cd "${SCRIPT_DIR}"
 #    cp -p ../zotero/chrome/content/zotero/xpcom/integration.js \
